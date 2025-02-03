13fdb40999ced948f58de74ac3d59a31c055bf27
Voppor41 <egorselifanov334@yandex.com>
2025-02-04 01:57:00 +0300

    Финальный результат

--- FastApi/backend/db_depends.py
+++
@@ -1,8 +0,0 @@
-from FastApi.backend.db import SessionLocal
-
-async def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
--- FastApi/models/user.py
+++
@@ -1,9 +0,0 @@
-from FastApi.backend.db import Base
-from sqlalchemy import Column, Integer, String
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    age = Column(Integer)
-    hashed_password = Column(String)
